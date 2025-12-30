a4d1e7cb82ee0b919322d2fc49694628eb04840a
Williams <williams.rm99@gmail.com>
2025-12-30 16:41:29 -0400

    feat(main): set default DNS result order to prioritize IPv4

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { setDefaultResultOrder } from 'dns';
+setDefaultResultOrder('ipv4first');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
